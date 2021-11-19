aef2d3b3ad4f5a39e1843a3067f74585b309edaf
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-18 22:26:28 -0300

    migrate Director

---
+++ Migrations/20211119012303_Director.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PracticoLaboratorio4.Migrations
+{
+    public partial class Director : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Directores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Biografia = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    UrlFoto = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Directores", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Directores");
+        }
+    }
+}
