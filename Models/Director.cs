abb6d2f958320ad982625245f0975e5255e763f7
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-18 22:19:16 -0300

    add Director model

---
+++ Models/Director.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace PracticoLaboratorio4.Models
+{
+    public class Director
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [StringLength(500)]
+        [MaxLength(500)]
+        public string Biografia { get; set; }
+
+        [MaxLength(1000)]
+        public string UrlFoto { get; set; }
+
+        [NotMapped]
+        public IFormFile Foto { get; set; }
+    }
+}
