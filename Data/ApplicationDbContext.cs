163fe82c4fbe6c7ae31afbb28a0b573bd5ddb2b4
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-18 22:39:49 -0300

    make Genero.Descripcion unique and case insensitive

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,6 +7,17 @@ namespace PracticoLaboratorio4.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) {}
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genero>()
+                .Property(g => g.Descripcion)
+                .HasColumnType("TEXT COLLATE NOCASE");
+
+            modelBuilder.Entity<Genero>()
+                .HasIndex(g => g.Descripcion)
+                .IsUnique();
+        }
+
         public DbSet<Genero> Generos { get; set; }
         public DbSet<Director> Directores { get; set; }
         public DbSet<Pelicula> Peliculas { get; set; }
