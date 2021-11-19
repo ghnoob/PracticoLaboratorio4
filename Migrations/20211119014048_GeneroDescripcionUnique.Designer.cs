c1d6554b4ec555bcc647efb2b3fe5e05eaa6879c
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-18 22:41:49 -0300

    migrate model

--- Migrations/20211119014048_GeneroDescripcionUnique.Designer.cs
+++ Migrations/20211119014048_GeneroDescripcionUnique.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PracticoLaboratorio4.Data;
 
 namespace PracticoLaboratorio4.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211119014048_GeneroDescripcionUnique")]
+    partial class GeneroDescripcionUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20211119014048_GeneroDescripcionUnique.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PracticoLaboratorio4.Migrations
+{
+    public partial class GeneroDescripcionUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Generos",
+                type: "TEXT COLLATE NOCASE",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Generos_Descripcion",
+                table: "Generos",
+                column: "Descripcion",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Generos_Descripcion",
+                table: "Generos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Generos",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT COLLATE NOCASE",
+                oldMaxLength: 20);
+        }
+    }
+}
