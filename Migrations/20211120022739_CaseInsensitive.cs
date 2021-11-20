f0c5067c49a0d6a3bea38b0ca4d37d9c6f0a66b8
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-19 23:29:32 -0300

    migrate model

---
+++ Migrations/20211120022739_CaseInsensitive.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PracticoLaboratorio4.Migrations
+{
+    public partial class CaseInsensitive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Peliculas",
+                type: "TEXT COLLATE NOCASE",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Directores",
+                type: "TEXT COLLATE NOCASE",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Peliculas",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT COLLATE NOCASE",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Directores",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT COLLATE NOCASE",
+                oldMaxLength: 50);
+        }
+    }
+}
